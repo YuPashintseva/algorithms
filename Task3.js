8bc9e7014f315c0b434a01a2114168417c9b4499
YuPashintseva <juliyapashintseva@gmail.com>
2021-02-08 17:48:18 +0600

    feat: add task3

---
+++ Task3.js
@@ -0,0 +1,14 @@
+const buttons = [
+    ['1', '2abc', '3def'],
+    ['4ghi', '5jkl', '6mno'],
+    ['7pqrs', '8tuv', '9wxyz'],
+    ['*', '0', '#']
+]
+
+function mobileRemote(text) {
+        // понять регистр буквы
+}
+
+
+console.log(mobileRemote('C')); // 10
+console.log(mobileRemote('yandex')); // 34
